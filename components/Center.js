edde2ba24fefc35e90e3581b18eead2956a2441f
munkh-erdene enkhbold <munkherdene.enkh@mobicom.mn>
2025-01-02 18:22:11 +0800

    center

--- components/Center.js
+++ components/Center.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 const StyledDiv = styled.div`
     max-width: 1100px;
     margin: 0 auto;
-    padding: 0 20px;
+    padding: 0 20px 20px;
 `;
 
 export default function Center({children}){
